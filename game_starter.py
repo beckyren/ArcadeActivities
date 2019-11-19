6e84420b1b90795a0bf02f4f12c6089e6eb22e6a
Becky <believesonamy4ever@gmail.com>
2019-11-19 11:12:12 -0500

    Another commit

--- game_starter.py
+++ game_starter.py
@@ -1,10 +1,11 @@
 import arcade
+import pymunk
 #Remember to credit authors
 
 
 # Define constants
-WINDOW_WIDTH = 500
-WINDOW_HEIGHT = 500
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 600
 BACKGROUND_IMG = "images/game_background.png"
 GAME_TITLE = "Meteor Garden"
 GAME_SPEED = 1/60
@@ -16,7 +17,7 @@ class Character(arcade.Sprite):
     def __init__(self):
         super().__init__()
         self.center_x = WINDOW_WIDTH / 2
-        self.center_y = WINDOW_HEIGHT / 2
+        self.center_y = WINDOW_HEIGHT / 6
         self.scale = 0.25
         '''code below is from Paul Vincent Craven via http://arcade.academy/examples/sprite_face_left_or_right.html'''
         texture = arcade.load_texture("images/main_facing_right.png", mirrored=True, scale=self.scale)
@@ -24,14 +25,21 @@ class Character(arcade.Sprite):
         texture = arcade.load_texture("images/main_facing_right.png", scale=self.scale)
         self.textures.append(texture)
         self.set_texture(FACING_RIGHT)
+
     def update_animation(self, delta_time: float = 1/60):
         '''code below is from Paul Vincent Craven via http://arcade.academy/examples/sprite_face_left_or_right.html'''
         self.center_x+=self.change_x
         self.center_y+=self.change_y
         if self.change_x<0:
-            self.set_texture(FACING_LEFT)
-        if self.change_x > 0:
             self.set_texture(FACING_RIGHT)
+        if self.change_x > 0:
+            self.set_texture(FACING_LEFT)
+        if self.right>WINDOW_WIDTH-1:
+            self.right=WINDOW_WIDTH-1
+        if self.left < 0:
+            self.left = 0
+        if self.top > WINDOW_HEIGHT - 1:
+            self.top = WINDOW_HEIGHT - 1
 
 
 
@@ -60,26 +68,25 @@ class MeteorGarden(arcade.Window):
         arcade.draw_texture_rectangle(WINDOW_WIDTH//2, WINDOW_HEIGHT//2,
                                       WINDOW_WIDTH, WINDOW_HEIGHT, self.background)
         self.character.draw()
-
+        arcade.draw_text("Enter your name",WINDOW_WIDTH/3,WINDOW_HEIGHT/3,arcade.color.LIGHT_CRIMSON)
         #draw_text
         #Physics engine
 
     def on_update(self, delta_time):
         """ Called every frame of the game (1/GAME_SPEED times per second)"""
-        self.character.update()
+        self.character.update_animation()
     def on_key_press(self, key, modifiers: int):
-        if key==arcade.key.UP:
-            self.character.change_y+=character_speed*2
-
-
-        elif key==arcade.key.LEFT:
+        if key==arcade.key.LEFT:
             self.character.change_x=-character_speed
         elif key==arcade.key.RIGHT:
             self.character.change_x=character_speed
+        elif key==arcade.key.UP:
+            self.character.change_y=character_speed
 
-    def on_key_release(self, key, modifiers: int):
-        if key==arcade.key.UP: 
-            self.character.change_y-=(character_speed)
+    def on_key_release(self, key, modifiers: int):#nestnig no
+        if key==arcade.key.UP:
+            self.character.change_y=0
+
         elif key==arcade.key.LEFT or key==arcade.key.RIGHT:
             self.character.change_x=0
 
@@ -90,6 +97,7 @@ def main():
     window = MeteorGarden()
     window.setup()
     arcade.run()
+
 
 
 if __name__ == "__main__":
