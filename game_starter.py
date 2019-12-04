4673dd52ef59a049ee97a29c39af309bc08632ed
Becky <believesonamy4ever@gmail.com>
2019-12-04 15:54:52 -0500

    Almost done now

--- game_starter.py
+++ game_starter.py
@@ -80,9 +80,6 @@ class Introduction(arcade.View):
 
 
 class MeteorGarden(arcade.View):
-    '''make a new list for leap potions, remove from collision list but move potion to new list.
-in on keypress function, if key is space bar and len of newlist is >0, character jump 300 pixels and newlist.pop
-to decreas length'''
     def __init__(self):
         """ Initialize variables """
         super().__init__()
@@ -112,6 +109,7 @@ to decreas length'''
         self.meteor_list = arcade.SpriteList()
         self.fallen_list = arcade.SpriteList()
         self.bottle_list = arcade.SpriteList()
+        self.collected_list = arcade.SpriteList()
 
     def on_draw(self):
         """ Called when it is time to draw the world """
@@ -163,8 +161,9 @@ to decreas length'''
             self.collision_list2 = arcade.check_for_collision_with_list(self.character, self.bottle_list)
             if len(self.collision_list2) > 0:
                 arcade.draw_text("Leap Potion", 0, 550, arcade.color.BLACK)
-                self.character.center_y = 300
+                #self.character.center_y = 300
                 bottle.remove_from_sprite_lists()
+                self.collected_list.append(bottle)
 
         self.fallen_list.update()
 
@@ -173,6 +172,9 @@ to decreas length'''
             self.window.show_view(game_over)
 
     def on_key_press(self, key, modifiers: int):
+        '''make a new list for leap potions, remove from collision list but move potion to new list.
+        in on keypress function, if key is space bar and len of newlist is >0, character jump 300 pixels and newlist.pop
+        to decreas length'''
         if key == arcade.key.LEFT:
             self.character.change_x = -character_speed
         elif key == arcade.key.RIGHT:
@@ -180,6 +182,10 @@ to decreas length'''
         elif key == arcade.key.UP:
             if self.physics.can_jump():
                 self.character.change_y = character_speed
+        elif key== arcade.key.SPACE and len(self.collected_list)>0:
+            self.character.center_y=350
+            self.collected_list.pop()
+
 
     def on_key_release(self, key, modifiers: int):  # nestnig no
         if key == arcade.key.UP:
